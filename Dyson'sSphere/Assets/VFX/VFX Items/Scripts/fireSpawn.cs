4821c642aed4be6153d448893db119b4d447a669
Trevor Petersen <tpetersen125@yahoo.com>
2016-04-20 21:52:18 -0600

    Here are all of the items used for the ice shot and fire pillar VFX.

---
+++ Dyson'sSphere/Assets/VFX/VFX Items/Scripts/fireSpawn.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fireSpawn : MonoBehaviour {
+
+	public GameObject fireGroup;
+	public float waitForIt;
+	private bool canPress;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		fireGroup.SetActive(false);
+		canPress = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Q) && canPress == true)
+		{
+			canPress = false;
+			fireGroup.SetActive(true);
+			StartCoroutine(stopFire());
+		}
+	}
+
+	private IEnumerator stopFire ()
+	{
+		yield return new WaitForSeconds (waitForIt);
+		fireGroup.SetActive(false);
+		canPress = true;
+	}
+}
