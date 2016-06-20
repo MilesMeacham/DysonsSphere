f5eb89fcb64bc6dd81369c49600cf3c25441a097
Miles <littlemeacham@gmail.com>
2016-06-20 16:14:42 -0600

    Fixing minor issues

---
+++ Dyson'sSphere/Assets/Camera/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	private Transform target;
+	public Vector3 cameraOffset;
+
+	void Start ()
+	{
+		target = GameObject.Find ("Player");
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		gameObject.transform.position = target.transform.position + cameraOffset;
+
+
+
+	}
+}
